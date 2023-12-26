49bc310bbd277ee66145871c3245e5a0563e9312
Poobly <79822237+Poobly@users.noreply.github.com>
2023-12-26 18:14:28 -0500

    fixed merge mistake

--- main.js
+++ main.js
@@ -25,16 +25,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
-  
-  app.whenReady().then(() => {
-    createWindow()
-  })
