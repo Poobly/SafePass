7745529017bdbe3f571d87f97c2000a55b334472
Poobly <79822237+Poobly@users.noreply.github.com>
2023-12-26 17:52:24 -0500

    specific opening/closing app functionality added

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    });
+  
+    win.loadFile('index.html');
+}
+
+// When app is ready creates window using function above
+app.whenReady().then(() => {
+  createWindow();
+
+  // Checks if app is activated and then creates window (macos)
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Closes the application if you exit all GUI windows 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
